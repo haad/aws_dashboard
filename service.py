4ce4adf17051f9304f5130eb53ff16f051013f82
Rahul Menon <rmenon@apigee.com>
2015-01-14 11:16:08 +0530

    Added Check for Subnets <15 Free Ips

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ import config
 import boto.ec2.elb
 import boto
 from boto.ec2 import *
+import boto.vpc
 
 
 app = Flask(__name__)
@@ -16,18 +17,30 @@ def index():
 
     list = []
     creds = config.get_ec2_conf()
+    key = creds['AWS_ACCESS_KEY_ID']
+    secret = creds['AWS_SECRET_ACCESS_KEY']
 
     for region in config.region_list():
-        conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+        conn = connect_to_region(region, aws_access_key_id=key, aws_secret_access_key=secret)
+        vpcconn = boto.vpc.connect_to_region(region, aws_access_key_id=key, aws_secret_access_key=secret)
         zones = conn.get_all_zones()
         instances = conn.get_all_instance_status(max_results=2000)
         instance_count = len(instances)
         ebs = conn.get_all_volumes()
         ebscount = len(ebs)
+        subnets = vpcconn.get_all_subnets()
         unattached_ebs = 0
         unattached_eli = 0
         event_count = 0
         improperelb = 0
+        subnet_counter = 0
+        ip_low_subnet = 0
+        subnet_counter = len(subnets)
+
+        for subnet in subnets:
+            if subnet.available_ip_address_count < 15:
+                ip_low_subnet = ip_low_subnet + 1
+
 
         for instance in instances:
             events = instance.events
@@ -54,7 +67,7 @@ def index():
         for elb in elbs:
             if len(elb.instances) < 2:
                 improperelb = improperelb + 1
-        list.append({ 'region' : region, 'zones': zones, 'instance_count' : instance_count, 'ebscount' : ebscount, 'unattached_ebs' : unattached_ebs, 'eli_count' : eli_count, 'unattached_eli' : unattached_eli, 'elb_count' : elb_count, 'event_count' : event_count, 'improper_elb': improperelb})
+        list.append({ 'region' : region, 'zones': zones, 'instance_count' : instance_count, 'ebscount' : ebscount, 'unattached_ebs' : unattached_ebs, 'eli_count' : eli_count, 'unattached_eli' : unattached_eli, 'elb_count' : elb_count, 'event_count' : event_count, 'improper_elb': improperelb, 'subnet_counter': subnet_counter, 'ip_low_subnet': ip_low_subnet})
 
     return render_template('index.html', list=list)
 
@@ -136,6 +149,18 @@ def elbimproper(region=None):
             elb_info = {'elb_name': elb.dns_name, 'elb_attached_instances': elb.instances, 'elb_healthcheck': elb.health_check}
             badelb.append(elb_info)
     return render_template('elb.html', badelb=badelb)
+
+@app.route('/lowipsubnet/<region>/')
+def lowipsubnet(region=None):
+    creds = config.get_ec2_conf()
+    vpcconn = boto.vpc.connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+    subnets = vpcconn.get_all_subnets()
+    lowsubnet = []
+    for subnet in subnets:
+        if subnet.available_ip_address_count < 15:
+            subnet_info = {'subnet_id': subnet.id, 'subnet_cidr': subnet.cidr_block, 'subnet_az': subnet.availability_zone, 'subnet_avail_ip': subnet.available_ip_address_count, 'subnet_tags': subnet.tags}
+            lowsubnet.append(subnet_info)
+    return render_template('subnet.html', lowsubnet=lowsubnet)
 
 
 if __name__ == '__main__':
