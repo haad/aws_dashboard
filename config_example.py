2bee8cb8b295b52ce0b78420899526a34e2f1b60
HAMSIK Adam <adh@rsd.com>
2015-04-08 17:50:16 +0200

    Use env variables to fetch aws keys install gunicorn as dep.

--- config_example.py
+++ config_example.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 # vim: set expandtab:
 import os
-def get_ec2_conf():
-    AWS_ACCESS_KEY_ID = 'YOUR ACCESS KEY ID'
-    AWS_SECRET_ACCESS_KEY = 'YOUR SECRET ACCESS KEY'
-    return {'AWS_ACCESS_KEY_ID' : AWS_ACCESS_KEY_ID, 'AWS_SECRET_ACCESS_KEY' : AWS_SECRET_ACCESS_KEY}
+def ec2_conf():
+    return {'AWS_ACCESS_KEY_ID' : os.environ.get('AWS_ACCESS_KEY_ID'), 'AWS_SECRET_ACCESS_KEY' : os.environ.get('AWS_SECRET_ACCESS_KEY')}
 
 def region_list():
     region_list = ['us-east-1','us-west-1','us-west-2']
